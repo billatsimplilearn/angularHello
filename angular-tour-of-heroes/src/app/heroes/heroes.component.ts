0b9850e450f0c03de15a7f5e388d6e0cd71e91af
Bill Milligan <public@billmilligan.com>
2023-08-06 13:58:19 -0400

    Tour of Heroes Part 4 is done showing Services, Observables, and subscriptions.  Also shows one service calling another

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero' ;
-import { HEROES } from '../mock-heroes' ;
+import { HeroService } from '../hero.service' ;
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,28 @@ import { HEROES } from '../mock-heroes' ;
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes : Hero [ ] = HEROES ;
+  heroes : Hero [ ] = [ ] ;
   selectedHero ?: Hero ;
+  private heroService: HeroService ;
+
+  /* auto wiring */
+  /* Making this different from Tour of Heroes to make it clearer but
+    it means the same thing */
+  constructor(heroService: HeroService) {
+    this.heroService = heroService ;
+  }
+
+  ngOnInit() : void {
+    this.populateHeroes();
+  }
 
   onSelect(hero : Hero) {
     this.selectedHero = hero;
+  }
+
+  // ToH calls this getHeroes():
+  populateHeroes() : void {
+      this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
